4849f9ed7943513be8de0d4e0b0b6c7ce6adeeeb
Dan <36828197+dangraham99@users.noreply.github.com>
2022-01-04 01:35:43 +0000

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex justify-center py-8">
+      <h1 className="font-semibold text-3xl">React and Firebase Authentication</h1>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./public/**/*.{js,ts,jsx,tsx,html}"
+
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Inter', 'Sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
