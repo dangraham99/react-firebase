a68d835bedb4eab6310e3126441d1bba27a83805
Dan <36828197+dangraham99@users.noreply.github.com>
2022-01-04 02:37:06 +0000

    switch to jsx extension

--- src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="App flex justify-center py-8">
-      <h1 className="font-semibold text-3xl">React and Firebase Authentication</h1>
-    </div>
-  );
-}
-
-export default App;
