ada4a7a7febe99b779323cf16a45477bb5c175db
Dan <36828197+dangraham99@users.noreply.github.com>
2022-01-05 02:33:36 +0000

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,42 @@
 import Login from "./components/Login";
 import Navbar from "./components/Navbar";
+import Register from "./components/Register";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App font-[Inter]">
-      <Navbar />
-      <div className="max-w-1/5">
-        <Login />
-      </div>
-    </div>
+
+      <Router>
+        <Navbar />
+        <div className="max-w-1/5">
+          <Routes>
+
+
+            <Route
+              path='/login'
+              element={
+                <>
+                  <Login />
+                </>
+              }
+            />
+
+            <Route
+              path='/register'
+              element={
+                <>
+                  <Register />
+                </>
+              }
+            />
+
+
+          </Routes>
+        </div>
+      </Router>
+
+    </div >
   );
 }
 
