794dd7465ca950b8ad519ad339c3d818a6c20636
Dan <36828197+dangraham99@users.noreply.github.com>
2022-01-05 01:06:56 +0000

    fonts

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
     return (
         <div>
-            <nav className="py-4 font-semibold ">
+            <nav className="py-4 font-semibold backdrop-blur-lg border-b-1 shadow-md bg-white border-neutral-300">
                 <div className="px-12 mx-auto xl:max-w-5xl xl:px-0">
                     <div className="flex items-center justify-between ">
                         <div className="flex space-x-12">
@@ -13,13 +13,13 @@ const Navbar = () => {
                                 </svg>
                                 <span>React-Firebase</span>
                             </a>
-                            <a href="#" className="border-b-2">Dashboard</a>
+                            <a href="#" className="border-b-2 border-neutral-500">Dashboard</a>
                             <a href="#">Tasks</a>
                         </div>
                         <div className="flex items-center justify-between space-x-6">
-                            <a href="#">Login</a>
-                            <a href="#" className='flex px-4 py-2 font-bold text-white bg-teal-600 rounded hover:bg-teal-800'>
-                                <span className="mr-1">Signup</span>
+                            <a href="#">Sign in</a>
+                            <a href="#" className='flex px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600'>
+                                <span className="mr-1">Sign up</span>
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
                                 </svg>
