f58bf0d15f19bf9b0d92912ed0b77c1dbdb0b154
Dan <36828197+dangraham99@users.noreply.github.com>
2022-01-06 03:23:09 +0000

    display form errors

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 const Form = (props) => {
     return (
         <div>
-            <h1 className="font-bold text-2xl py-8 max-w-sm mx-auto text-center">{props.formTitle}</h1>
-            <form action={props.formAction} className="mx-auto bg-white text-gray-700 font-semibold shadow-md rounded-md px-8 pt-6 pb-8 mb-4 max-w-sm ">
+            <h1 className="max-w-sm py-8 mx-auto text-2xl font-bold text-center">{props.formTitle}</h1>
+            <form method="post" onSubmit={props.onSubmit} className="max-w-sm px-8 pt-6 pb-8 mx-auto mb-4 font-semibold text-gray-700 bg-white rounded-md shadow-md ">
+                {props.formError && <div className="p-3 my-3 text-sm text-center text-red-700 bg-red-200 rounded">{props.formError}</div>}
                 {props.children}
             </form>
         </div>
