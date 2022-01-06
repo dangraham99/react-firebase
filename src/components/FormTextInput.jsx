9c6404d9f14c93a2cc67bd7ea9d4a20ff6850417
Dan <36828197+dangraham99@users.noreply.github.com>
2022-01-06 03:24:05 +0000

    add state and ref to form inputs

--- src/components/FormTextInput.jsx
+++ src/components/FormTextInput.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const FormTextInput = ({ inputName, inputType, labelText }) => {
+const FormTextInput = ({ inputName, inputType, labelText, inputRef }) => {
     return (
         <div className="mb-4">
             <label htmlFor="" className="text-xs uppercase">{labelText}</label>
-            <input name={inputName} type={inputType} className="appearance-none border focus:outline-blue-200 focus:shadow-outline border-gray-200 rounded w-full py-2 px-3" />
+            <input name={inputName} type={inputType} ref={inputRef} className="w-full px-3 py-2 border border-gray-200 rounded appearance-none focus:outline-blue-200 focus:shadow-outline" />
         </div>
     )
 }
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,21 +1,54 @@
-import React from 'react'
+import React, {useRef, useState} from 'react'
 import Form from './Form'
 import FormTextInput from './FormTextInput'
 import FormSubmitBtn from './FormSubmitBtn'
 import FormInlineLink from './FormInlineLink'
+import { useAuth } from '../contexts/AuthContext'
 
 const Register = () => {
+
+    const nameRef = useRef()
+    const emailRef = useRef()
+    const passwordRef = useRef()
+    const passwordConfirmRef = useRef()
+    const { register } = useAuth()
+    const [formError, setFormError] = useState('')
+    const [loading, setLoading] = useState(false)
+
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+
+        //check confirm password
+        if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+            return setFormError('Passwords do not match.')
+        }
+
+        setLoading(true)
+
+        try {
+            await register(emailRef.current.value, passwordRef.current.value)
+        } catch {
+            return setFormError('Account registration failed!')
+            
+        }
+        
+        setLoading(false)
+    }
+
     return (
         <div>
-            <Form formTitle="Let's get started">
+            <Form onSubmit={handleSubmit} formTitle="Let's get started" formError={formError} method="">
 
-                <FormTextInput inputName="email" inputType="email" labelText="email" />
-                <FormTextInput inputName="password" inputType="password" labelText="Password" />
-                <FormTextInput inputName="password_confirm" inputType="password_confirm" labelText="Confirm Password" />
+                <FormTextInput inputName="name" inputType="text" labelText="name" inputRef={nameRef} />
+                <FormTextInput inputName="email" inputType="email" labelText="email" inputRef={emailRef} />
+                <FormTextInput inputName="password" inputType="password" labelText="Password" inputRef={passwordRef} />
+                <FormTextInput inputName="password_confirm" inputType="password" labelText="Confirm Password" inputRef={passwordConfirmRef} />
 
-                <FormSubmitBtn buttonText="Create account" hrefLocation="#" />
+                
 
-                <div className="text-center">
+                <div className="pt-1 text-center">
+                    <FormSubmitBtn disabled={loading} buttonText="Create account" hrefLocation="#" />
                     <FormInlineLink inlineText="Already have an account? " linkText="Sign in." hrefLocation='/login' />
                 </div>
 
