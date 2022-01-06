af63e06f55da46c97b63df3f8fa2248b74afcd34
Dan <36828197+dangraham99@users.noreply.github.com>
2022-01-06 03:23:46 +0000

    disable submit button on load

--- src/components/FormSubmitBtn.jsx
+++ src/components/FormSubmitBtn.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const FormSubmitBtn = ({ buttonText, hrefLocation }) => {
+const FormSubmitBtn = ({ disabled, buttonText, hrefLocation }) => {
     return (
         <div className="flex justify-center mt-4">
-            <input type="submit" value={buttonText} href={hrefLocation} className='w-full cursor-pointer px-4 py-2 font-bold text-white bg-blue-500 border-neutral-600 border-1 rounded hover:bg-blue-600' />
+            <input type="submit" disabled={disabled} value={buttonText} href={hrefLocation} className='w-full px-4 py-2 font-bold text-white bg-blue-500 rounded cursor-pointer border-neutral-600 border-1 hover:bg-blue-600' />
         </div>
     )
 }
