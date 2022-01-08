58242a3295d99f9ff55f08a494f8aeba71de894f
Dan <36828197+dangraham99@users.noreply.github.com>
2022-01-08 02:08:50 +0000

    spacing

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -20,13 +20,13 @@ function Dashboard() {
         <Content pageTitle="Dashboard">
             <div className="max-w-md px-8 pt-6 pb-8 my-8 text-gray-700 bg-white rounded-md shadow-md">
                 <h1 className="pb-4 mx-auto text-2xl font-bold">Profile</h1>
-                <div className="space-y-1 mb-4">
-                    {currentUser.photoURL && <img src={currentUser.photoURL} className="rounded-full border-2 border-gray-200 hover:shadow-lg my-2" />}
+                <div className="mb-4 space-y-1">
+                    {currentUser.photoURL && <img src={currentUser.photoURL} className="my-2 border-2 border-gray-200 rounded-full" />}
                     {currentUser.displayName && <p><span className='font-semibold'>Name:</span> {currentUser.displayName}</p>}
                     <p><span className='font-semibold'>Email:</span> {currentUser.email}</p>
 
                 </div>
-                <div className="space-y-3 pt-5">
+                <div className="pt-5 space-y-3">
                     <Link to="/update-profile" className='w-full px-4 py-2 font-bold text-white bg-blue-500 rounded cursor-pointer border-neutral-600 border-1 hover:bg-blue-600'>Edit profile</Link>
                 </div>
             </div>
